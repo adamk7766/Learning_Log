d7e7662404763f54f503a237ae2cc78492fbf78f
adamk7766@gmail.com <adamk7766@gmail.com>
2018-08-04 17:53:55 +0200

    First commit

---
+++ learning_logs/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+from datetime import datetime
+
+# Create your models here.
+
+class Topic(models.Model):
+    """Temat poznawany przez użytkownika."""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        """Zwraca reprezentację modelu w postaci ciągu tekstowego."""
+        return self.text
+
+class Entry(models.Model):
+    """Konkretne informacje o postępie w nauce."""
+    topic = models.ForeignKey(Topic,
+        on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Zwraca reprezentację modelu w postaci ciągu tekstowego."""
+        text_to_show = ''
+        if len(self.text) > 50:
+            text_to_show = self.text[:50] + "..."
+        else:
+            text_to_show = self.text[:50]    
+        return text_to_show
